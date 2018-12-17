c51fe31cd2c887bb4d38ae3bacf098c3af2fa58e
Yamel Senih (http://erpya.com/) <ysenih@erpya.com>
2018-12-17 13:45:21 -0400

    Change license

--- Device-Management/src/main/java/org/erpya/base/device/util/GenericDeviceHandler.java
+++ Device-Management/src/main/java/org/erpya/base/device/util/GenericDeviceHandler.java
@@ -1,19 +1,19 @@
-/******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * This program is free software; you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
- * You should have received a copy of the GNU General Public License along    *
- * with this program; if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- * For the text or an alternative of this public license, you may reach us    *
- * Copyright (C) 2003-2016 E.R.P. Consultores y Asociados.                    *
- * All Rights Reserved.                                                       *
- * Contributor(s): Yamel Senih www.erpcya.com                                 *
- *****************************************************************************/
+/*************************************************************************************
+ * Product: Spin-Suite (Mobile Suite)                       		                 *
+ * Copyright (C) 2012-2018 E.R.P. Consultores y Asociados, C.A.                      *
+ * Contributor(s): Yamel Senih ysenih@erpya.com				  		                 *
+ * Contributor(s): Carlos Parada cparada@erpya.com				  		             *
+ * This program is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by              *
+ * the Free Software Foundation, either version 3 of the License, or                 *
+ * (at your option) any later version.                                               *
+ * This program is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                    *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     *
+ * GNU General Public License for more details.                                      *
+ * You should have received a copy of the GNU General Public License                 *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
 package org.erpya.base.device.util;
 
 import android.content.Context;
